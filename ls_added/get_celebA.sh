ef236403b6984c1878a61bfa6d5e272d981ed381
EC2 Default User <ec2-user@ip-172-16-81-196.us-east-2.compute.internal>
2019-12-03 06:47:50 +0000

    Adding shell script to get celebA dataset

---
+++ ls_added/get_celebA.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+celeba_url="https://doc-0s-84-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/9j1a5na7nrv2b4hcqr0unv8st65cqilf/1574992800000/13182073909007362810/*/0B7EVK8r0v71pZjFTYXZWM3FlRnM?e=download"
+
+wget -O celebA $celeba_url
+unzip celebA
+rm -R celebA
